a7161f7035e29fb7cbd35967b3f4a3ffe04ed5e0
Atlas <1562718553@qq.com>
2016-01-31 01:02:10 +0800

    change the listselector

--- app/src/main/java/com/zju/yibao/CourseActivity.java
+++ app/src/main/java/com/zju/yibao/CourseActivity.java
@@ -2,10 +2,8 @@ package com.zju.yibao;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
@@ -31,38 +29,6 @@ public class CourseActivity extends Activity {
 
     }
 
-    private void fillComment(Course course) {
-        ListView listView = (ListView) findViewById(R.id.list_comment);
-        List<Map<String, Object>> listItems = new ArrayList<>();
-        String[] strings = new String[]{"studentName", "comment", "studentHeadPortraits", "starLevel"};
-//        String[] strings = new String[]{"studentName", "comment", "studentHeadPortraits"};
-        for (int i = 0; i < course.getCourseCommentViews().size(); i++) {
-            Map<String, Object> item = new HashMap<>();
-            item.put(strings[0], course.getCourseCommentViews().get(i).getStudentName());
-            item.put(strings[1], course.getCourseCommentViews().get(i).getComment());
-            item.put(strings[2], R.drawable.user);
-            item.put(strings[3], course.getCourseCommentViews().get(i).getStarLevel());
-
-            listItems.add(item);
-        }
-        MySimpleAdapter adapter = new MySimpleAdapter(
-                CourseActivity.this,
-                listItems,
-                R.layout.list_item_comment,
-                strings,
-                new int[]{R.id.tv_studentName, R.id.tv_comment, R.id.img_studentHeadPortraits, R.id.rating_starLevel}
-//                new int[]{R.id.tv_studentName, R.id.tv_comment, R.id.img_studentHeadPortraits}
-        );
-
-        ViewGroup.LayoutParams params = listView.getLayoutParams();
-
-        params.height *= course.getCourseCommentViews().size();
-        listView.setLayoutParams(params);
-
-        listView.setAdapter(adapter);
-    }
-
-
     private void fillData(Course course) {
         fillString(course.getCourseName(), R.id.tv_course_name);
         fillString(course.getTeacherName(), R.id.tv_course_teacher);
@@ -82,6 +48,39 @@ public class CourseActivity extends Activity {
 
     private void fillString(String string, int id) {
         ((TextView) findViewById(id)).setText(string);
+    }
+
+    private void fillComment(Course course) {
+        ListView listView = (ListView) findViewById(R.id.list_comment);
+
+        List<Map<String, Object>> listItems = new ArrayList<>();
+
+        String[] strings = new String[]{"studentName", "comment", "studentHeadPortraits", "starLevel"};
+        int[] to = {R.id.tv_studentName, R.id.tv_comment, R.id.img_studentHeadPortraits, R.id.rating_starLevel};
+
+        for (int i = 0; i < course.getCourseCommentViews().size(); i++) {
+            Map<String, Object> item = new HashMap<>();
+            item.put(strings[0], course.getCourseCommentViews().get(i).getStudentName());
+            item.put(strings[1], course.getCourseCommentViews().get(i).getComment());
+            item.put(strings[2], R.drawable.user);
+            item.put(strings[3], course.getCourseCommentViews().get(i).getStarLevel());
+
+            listItems.add(item);
+        }
+
+        MySimpleAdapter adapter = new MySimpleAdapter(
+                CourseActivity.this,
+                listItems,
+                R.layout.list_item_comment,
+                strings,
+                to
+        );
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height *= course.getCourseCommentViews().size();
+        listView.setLayoutParams(params);
+
+        listView.setAdapter(adapter);
     }
 
 }
